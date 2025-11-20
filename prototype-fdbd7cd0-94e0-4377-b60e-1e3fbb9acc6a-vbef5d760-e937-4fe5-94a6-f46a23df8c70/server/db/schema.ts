670d1c2d3c2e49e3837e03c8f0a2458697208193
jptsung94 <joshptsung@gmail.com>
2025-11-19 20:34:39 -0500

    Delete prototype-fdbd7cd0-94e0-4377-b60e-1e3fbb9acc6a-vbef5d760-e937-4fe5-94a6-f46a23df8c70 directory

--- prototype-fdbd7cd0-94e0-4377-b60e-1e3fbb9acc6a-vbef5d760-e937-4fe5-94a6-f46a23df8c70/server/db/schema.ts
+++
@@ -1,6 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-
-export const testing = sqliteTable('testing', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  created_at: text('created_at').notNull().default('CURRENT_TIMESTAMP'),
-});
